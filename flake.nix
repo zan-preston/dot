c1d3acd3824380d432432e87e77c002a20d9c77a
Zan Preston <preston.zan@gmail.com>
2022-09-08 14:30:20 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Home Manager configuration of Zan Preston";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      # `home-manager switch --flake '.config#zan'`
+      homeConfigurations.zan = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [
+          ./nixpkgs/home.nix
+        ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
