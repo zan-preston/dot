0d5ce4d8957479b04271c9a9fbaa70422d5bdf03
Zan Preston <zan.preston@banno.com>
2022-09-15 16:42:30 -0500

    nightly neovim

--- flake.nix
+++ flake.nix
@@ -8,20 +8,29 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, neovim-nightly-overlay, ... }:
     let
       system = "x86_64-darwin";
+      overlays = [
+        neovim-nightly-overlay.overlay
+      ];
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       # `home-manager switch --flake '.config#zan'`
       homeConfigurations.zan = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
         modules = [
+          ({config, pkgs, ... }:
+            { nixpkgs.overlays = overlays;
+            })
           ./nixpkgs/home.nix
         ];
 
