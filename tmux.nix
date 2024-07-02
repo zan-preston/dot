15d7ff22ce5e8aa5517684066af727590f4f91b3
Zan Preston <zan.preston@banno.com>
2024-07-02 09:03:43 -0500

    tmux: add more configs

--- tmux.nix
+++ tmux.nix
@@ -4,15 +4,18 @@
       shell = "${pkgs.zsh}/bin/zsh";
       terminal = "tmux-256color";
       historyLimit = 10000;
+      escapeTime = 10;
       mouse = true;
       plugins = with pkgs; [
         tmuxPlugins.rose-pine
       ];
+      shortcut = "space";
       extraConfig = ''
       set -g @rose_pine_variant 'moon'
+      setw -g mode-keys vi
       bind | split-window -h
       bind - split-window -v
-      bind r source-file "~/.config/tmux/tmux.conf" && echo "Config Reloaded!"
+      bind r source-file ~/.config/tmux/tmux.conf \;  display "Config Reloaded!"
       '';
     };
 }
