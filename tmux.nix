7c19cd21f49c418a814a5043589409d066c54328
Zan Preston <zan.preston@banno.com>
2024-07-02 11:35:17 -0500

    tmux: configs

--- tmux.nix
+++ tmux.nix
@@ -12,9 +12,14 @@
       shortcut = "space";
       extraConfig = ''
       set -g @rose_pine_variant 'moon'
+      set -g status-position top
       setw -g mode-keys vi
       bind | split-window -h
       bind - split-window -v
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
       bind r source-file ~/.config/tmux/tmux.conf \;  display "Config Reloaded!"
       '';
     };
